5045f32a61a0f4b72424d87361d8df530a646fc5
amosansah-gh <amosansah44@gmail.com>
2023-06-15 15:53:09 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C command to printf
+ * Return: 0 (Access granted)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
