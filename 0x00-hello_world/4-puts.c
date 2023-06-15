73e09b3516a5decbfc98873166f117299dba5e7d
amosansah-gh <amosansah44@gmail.com>
2023-06-15 15:40:37 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A put function program
+ * Return: 0 (access granted)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
